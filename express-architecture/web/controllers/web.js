beb4ff3abe4e18d4f076c33b31d5fb0fa82c44f1
lujianan <ljn6176@163.com>
2017-04-10 21:03:43 +0800

    test

--- express-architecture/web/controllers/web.js
+++ express-architecture/web/controllers/web.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const datacenter = require('../lib/datacenter');
+const getDataForRenderPage = datacenter.getDataForRenderPage;
+const apiPipe = datacenter.apiPipe;
+
 module.exports = {
   home,
   getUserInfo
 };
 
 function home(req, res, next) {
-  res.render('index');
+  let dataInterfaces = [{
+    url: '/test',
+    params: {},
+    resName: 'test'
+  }];
+  return getDataForRenderPage(res, dataInterfaces)
+    .then(() => next({view: 'web/index'}))
+    .catch(err => {
+      logger.error(err);
+      next({status: 'pageNotFound', view: '404'});
+    });
 }
 
 function getUserInfo(req, res, next) {
