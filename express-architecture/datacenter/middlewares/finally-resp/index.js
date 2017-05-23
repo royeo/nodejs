d5cb3a57417a5eb22da8cfcaaaa01dc7a8f0947d
lujianan <ljn6176@163.com>
2017-05-23 14:10:29 +0800

    test

--- express-architecture/datacenter/middlewares/finally-resp/index.js
+++ express-architecture/datacenter/middlewares/finally-resp/index.js
@@ -8,12 +8,36 @@ const codeToStatus = require('./defines').codeToStatus;
 const JSONSTRING = 'JSONString';
 const ENCONDING = 'utf8';
 
+/**
+ * finallyResp
+ * @param {Object} options
+ * @param {Object} [options.format='JSONString'] - 默认接口返回的数据格式 JSON 或 JSONString
+ * @param {Object} [options.enconding='utf8']    - 默认接口返回的数据编码
+ * @param {Object} [options.views]               - 默认模板，如 view[500] = '500.ejs'
+ * @returns {Function}
+ */
 module.exports = function (options) {
   options = options || {};
   let defaultFormat = options.format || JSONSTRING;
   let enconding = options.enconding || ENCONDING;
   let views = options.views;
 
+  /**
+   * finallyResp
+   * @param {Object}          result              - 处理前的结果对象
+   * @param {String}          result.status       - 状态
+   * @param {*}               result.msg          - 数据
+   * @param {*}               result.ext          - 扩展
+   * @param {Error|String}    result.err          - 错误
+   * @param {String}          result.desc         - 描述
+   * @param {String}          result.view         - 视图模板（渲染成功）
+   * @param {String}          result.errorView    - 视图模板（渲染出错）
+   * @param {String}          result.page         - 静态文件路径
+   * @param {http.Request}    req                 - http.Request
+   * @param {http.Reponse}    res                 - http.Response
+   * @param {Function}        next                - app.next
+   * @returns {*}
+   */
   return function finallyResp(result, req, res, next) {
     if (_.isError(result)) {
       result = {
@@ -30,12 +54,12 @@ module.exports = function (options) {
       throw new Error('result.code undefined!');
     }
 
-    let url = req.url;
-    let msg = result.msg || final.desc;
-    let ext = result.ext || {};
+    let url  = req.url;
+    let msg  = result.msg || final.desc;
+    let ext  = result.ext || {};
     let view = result.view || views[final.statusCode] || final.view;
     let page = result.page;
-    let err = result.err;
+    let err  = result.err;
     let desc = result.desc || final.desc;
 
     function dealError(statusCode, err, view) {
@@ -96,7 +120,7 @@ module.exports = function (options) {
 
 function logError(url, err) {
   if (err instanceof Error || _.isError(err)) {
-    logger.error(`\n\n--------------------Error Begin--------------------\n\n${url}\n${err.stack}\n\n--------------------Error End--------------------\n`);
+    logger.error(`\n\nError Begin\n\n${url}\n${err.stack}\n\nError End\n`);
   } else {
     logger.warn(`\nWarn Begin\n#${err}\n${url}\nWarn End`);
   }
